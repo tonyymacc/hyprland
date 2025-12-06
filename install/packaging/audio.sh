57340f5558a1543635bcc1febdf0355a6371bfe7
Tony Leonardo <111930328+tonyymacc@users.noreply.github.com>
2025-12-05 22:06:44 -0600

    Change package installation command to use sudo

--- install/packaging/audio.sh
+++ install/packaging/audio.sh
@@ -1,1 +1,1 @@
-paru -S pamixer pipewire pipewire-alsa pipewire-jack pipewire-pulse wiremix wireplumber
+sudo pacman -S pamixer pipewire pipewire-alsa pipewire-jack pipewire-pulse wiremix wireplumber
